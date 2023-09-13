df12eb0856f7f70f0f23a18f5d96d96404fcd7cf
Yash Gupta <130299506+Yashg4454@users.noreply.github.com>
2023-09-13 14:30:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("mousemove", function(details){
+    var xval = gsap.utils.mapRange(0, window.innerWidth, 100, window.innerWidth-100, details.clientX);
+    gsap.to("#rect",{
+        left: xval + "px",
+        ease: Power3
+    });
+});
